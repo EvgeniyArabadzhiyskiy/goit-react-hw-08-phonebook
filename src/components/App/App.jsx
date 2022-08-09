fa8319a12f7da8c07a6db215d1c6d0cd8c54b8bc
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-09 19:55:20 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,7 +6,6 @@ import ContactPage from 'pages/ContactPage/ContactPage';
 import HomePage from 'pages/HomePage/HomePage';
 import { Route, Routes } from 'react-router-dom';
 
-
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import userOperations from 'redux/auth/auth-operations';
@@ -28,11 +27,24 @@ const App = () => {
         <>
           <Routes>
             <Route path="/" element={<SharedLayout />}>
-
-              <Route index element={<PublicRoute> <HomePage /> </PublicRoute>} />
-              <Route path="about" element={<PublicRoute><About /></PublicRoute>} />
+              <Route
+                index
+                element={
+                  <PublicRoute>
+                    {' '}
+                    <HomePage />{' '}
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="about"
+                element={
+                  <PublicRoute>
+                    <About />
+                  </PublicRoute>
+                }
+              />
               <Route path="/contacts" element={<ContactPage />} />
-
             </Route>
           </Routes>
         </>
@@ -43,43 +55,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import RegisterForm from 'components/RegisterForm/RegisterForm';
-// import LoginForm from 'components/LoginForm/LoginForm';
-// import PrivatRoute from 'components/Routes/PrivatRoute';
-
-{/* <Routes>
-            <Route path="/" element={<SharedLayout />}>
-              <Route index element={<HomePage />} />
-
-              <Route index element={<PublicRoute> <HomePage /> </PublicRoute>} />
-              <Route path="about" element={<PublicRoute><About /></PublicRoute>} />
-
-              <Route path="/contacts" element={<ContactPage />} />
-              <Route path="/contacts" element={<PrivatRoute Component={<ContactPage />} />} />
-              <Route path="/contacts" element={<PrivatRoute navigateTo="/login" />}>
-                <Route  path='/contacts' element={<ContactPage />} />
-              </Route>
-              <Route path="contacts" element={<PrivatRoute navigateTo="/about"> <ContactPage /> </PrivatRoute>} />
-
-
-              <Route path="/register" clousedForLogInUser element={<RegisterForm />} />
-              <Route path="/login" clousedForLogInUser element={<LoginForm />} />
-              <Route path="register"  element={<PublicRoute restricted navigateTo='/contacts'><RegisterForm /></PublicRoute>} />
-              <Route path="login"  element={<PublicRoute restricted navigateTo='/contacts'><LoginForm /></PublicRoute>} />
-              
-            </Route>
-          </Routes>
-        </> */}
