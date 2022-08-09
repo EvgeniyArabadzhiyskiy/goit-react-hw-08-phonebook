6902223d66d4265d96fc3e5233956fea843d024a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-09 19:55:00 +0300

    src/components/UserMenu/

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,34 @@
+import { useDispatch, useSelector } from 'react-redux';
+import authSelectors from 'redux/auth/auth-selectors';
+import userOperations from 'redux/auth/auth-operations';
+import { useNavigate } from 'react-router-dom';
+import { LoguotBtn, UserName } from './UserMenu.styled';
+
+const UserMenu = () => {
+  const dispatch = useDispatch();
+  const name = useSelector(authSelectors.getName);
+
+  const isLoggedIn = useSelector(authSelectors.getIsLOggedIn);
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    dispatch(userOperations.logOut());
+
+    if (!isLoggedIn) {
+      navigate('/', { replace: true });
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <LoguotBtn type="button" onClick={logOut}>
+          Logout
+        </LoguotBtn>
+        <UserName>Hi, {name}</UserName>
+      </div>
+    </>
+  );
+};
+
+export default UserMenu;
