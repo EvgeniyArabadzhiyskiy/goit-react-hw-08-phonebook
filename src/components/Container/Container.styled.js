2c614e75a0c696a0b9a9a61f89b2a98c4b143bfe
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-09 19:55:33 +0300

    Update Container.styled.js

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -17,12 +17,4 @@ export const StyledContainer = styled.div`
   @media screen and (min-width: 1200px) {
     width: 1200px;
   }
-
-  background-image: linear-gradient(
-      to right,
-      rgba(47, 48, 58, 0.2),
-      rgba(47, 48, 58, 0.2)
-    ),
-    url(${p => p.svg});
-  background-color: #b1aeae89;
 `;
