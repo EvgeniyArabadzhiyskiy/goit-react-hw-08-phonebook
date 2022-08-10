08bdae34393d0c3ddfb5d214e5ebad0df88f17cf
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-10 16:19:27 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -31,8 +31,7 @@ const App = () => {
                 index
                 element={
                   <PublicRoute>
-                    {' '}
-                    <HomePage />{' '}
+                    <HomePage />
                   </PublicRoute>
                 }
               />
